13214f554d89f374735b8cfb727c042f4ea30220
bistik <erickbianes@gmail.com>
2025-03-06 19:52:18 +0800

    Submission for exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const toggleVisibility = () => setVisible(!visible);
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  return (
+    <div className="blog-border">
+      <div>
+        {blog.title} {blog.author}
+        <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+        <button style={showWhenVisible} onClick={toggleVisibility}>hide</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+       </div>
+    </div>
+  )
+}
 
 export default Blog
