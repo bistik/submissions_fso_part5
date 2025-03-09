6faad2a83f3a207c586e3412e93448cbe62cbf14
bistik <erickbianes@gmail.com>
2025-03-09 09:18:28 +0800

    Submission for exercises 5.15 and 5.16

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import Blog from "./Blog"
-import { expect, vi } from "vitest"
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
+import BlogForm from './BlogForm'
+import { expect, vi } from 'vitest'
 
 test('test blog title and author are rendered', async () => {
   const blog = {
@@ -62,4 +63,53 @@ test('test blog url and likes are shown', async () => {
   expect(details).not.toHaveStyle('display: none')
   expect(hideButton).not.toHaveStyle('display: none')
   expect(viewButton).toHaveStyle('display: none')
+})
+
+test('multiple like clicks', async () => {
+  const blog = {
+    'title': 'Lorem impsum',
+    'author': 'John Doe',
+    'url': 'https://example.com/a-test',
+    'likes': 100,
+    'user': {
+      'username': 'foo',
+      'name': 'bar',
+      'id': '67c1a1f1b4c6c465bac79873'
+    },
+    'id': '67c1a35c985f1c510945b9fc'
+  }
+  const mockHandlerLike = vi.fn()
+  const mockHandlerDelete = vi.fn()
+
+  const { container } = render(<Blog blog={blog} loginUser={blog.user} handleLike={mockHandlerLike} handleDelete={mockHandlerDelete} />)
+
+  const user = userEvent.setup()
+  const viewButton = container.querySelector('.viewButton')
+  await user.click(viewButton)
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+  expect(mockHandlerLike).toHaveBeenCalledTimes(2)
+  expect(mockHandlerLike.mock.calls).toHaveLength(2)
+})
+
+test('test new blog form', async () => {
+  const newBlog = { title: '', author: '', url: '' }
+  const mockHandlerNewBlog = vi.fn()
+  const mockHandlerNewBlogInputChange = vi.fn()
+  const { container } = render(<BlogForm newBlog={newBlog} handleNewBlog={mockHandlerNewBlog} handleNewBlogInputChange={mockHandlerNewBlogInputChange} />)
+  const user = userEvent.setup()
+
+  const titleInput = container.querySelector('input[name="title"]')
+  const authorInput = container.querySelector('input[name="author"]')
+  const urlInput = container.querySelector('input[name="url"]')
+  const submitButton = container.querySelector('button[type="submit"]')
+  await user.type(titleInput, '1')
+  await user.type(authorInput, '2')
+  await user.type(urlInput, '3')
+  expect(mockHandlerNewBlogInputChange).toHaveBeenCalledTimes(3)
+  expect(mockHandlerNewBlogInputChange.mock.calls).toHaveLength(3)
+  await user.click(submitButton)
+  expect(mockHandlerNewBlog).toHaveBeenCalledTimes(1)
+  expect(mockHandlerNewBlog.mock.calls).toHaveLength(1)
 })
